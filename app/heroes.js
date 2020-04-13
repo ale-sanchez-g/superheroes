eb5e9b05ba3e674bf199f70d3ec8e3822f5ec3da
Alejandro <morsisdivine@gmail.com>
2020-04-14 08:17:21 +1000

    change the use of the API and created places and questions

--- app/heroes.js
+++ app/heroes.js
@@ -1,50 +1,38 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const office = require('./battlefield/office');
 const port = process.argv.slice(2)[0];
 const app = express();
 app.use(bodyParser.json());
 
 
-const heroes = [
-   {
-       id: 1,
-       type: 'handWasher',
-       displayName: 'The HandWasher',
-       img: 'handWasher.jpg'
-   },
-   {
-       id: 2,
-       type: 'social-distance',
-       displayName: 'Invisible Distance',
-       img: 'social-distance.jpg'
-   },
-   {
-       id: 3,
-       type: 'soap',
-       displayName: 'SOAP',
-       img: 'soap.jpg'
-   },
-   {
-       id: 4,
-       type: 'working-from-home',
-       displayName: 'WFH',
-       img: 'working-from-home.jpg'
-   }
+const battlefields = [
+        {place:"office"},
+        {place:"bus"},
+        {place:"restaurant"},
+        {place:"library"},
 ];
 
 app.get('/', (req, res) => {
+    // I want to add Swagger in the home page
     console.log('simple info on how to use the API');
-    res.send("To user go a GET request to /heroes");
+    res.send(battlefields);
  });
 
-app.get('/heroes', (req, res) => {
-   console.log('Returning heroes list');
-   res.send(heroes);
-});
+// app.get('/office', (req, res) => {
+//    console.log('Returning office questions');
+//    res.send(office);
+// });
   
-app.use('/img', express.static(path.join(__dirname,'img')));
+battlefields.forEach(function(battle){
+    const place = require('./battlefield/'+battle.place);
+    app.get(`/${battle.place}`, (req, res) => {
+        console.log(`Returning ${battle.place} questions`);
+        res.send(place);
+     });
+  });
+
 
 console.log(`Heroes service listening on port ${port}`);
 app.listen(port);

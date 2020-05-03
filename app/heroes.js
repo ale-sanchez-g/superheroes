da3ec6f5b6e281f14d415eb1a5032a66b052017f
sirhazman07 <haroldbaldwin@hotmail.com>
2020-05-03 15:49:19 +1000

    initial Post v2 submission - in progress

--- app/heroes.js
+++ app/heroes.js
@@ -16,6 +16,7 @@ const battlefields = [
         {place:"library"},
 ];
 
+//e.g GET: http://localhost:3000/
 app.get('/', function (req, res) {
     if (req.get('Authorization') == key ) {
         res.send(battlefields)
@@ -25,7 +26,7 @@ app.get('/', function (req, res) {
        } 
 })
 
-
+//e.g GET: http://localhost:3000/api/v2/office
 battlefields.forEach(function(battle){
     const place = require('./battlefield/v2/'+battle.place);
     app.get(`/api/v2/${battle.place}`, (req, res) => {
@@ -34,6 +35,31 @@ battlefields.forEach(function(battle){
     });
 });
 
+//e.g POST: http://localhost:3000/api/submitAnswer
+app.post(`/api/v2/submitAnswer`, function (req, res) {
+    console.log('Received data: ' + req)
+    res.send('POST request to the homepage')
+  });
+
+// //TODO: Create and move this logic to a Service (SOA)
+// function validateAnswer(question_id, answer) {
+//     let pass = false;
+//     const correct_answers = 
+//     [
+//         { bus_1: "Use your superheroes Mask and sanitizer while traveling on public transport and clean your hands regularly." },
+//         { bus_2: "Move to another seat immediately and report it to the driver!" },
+//         { bus_3:"Cover any sneezes or coughs with your superhero inner elbow or with a tissue that is then discarded."}
+//         // {place:"library"},
+//     ];
+//     //get the Question Id
+//     let match = correct_answers.findIndex(element => element.includes(question_id))
+//         alert (match);     
+//         //Validate Question Id against correct Answer
+//         if (answer === match) {
+//             pass = true;
+//         }        
+//     return pass
+// }
 
 console.log(`Covid Heroes service listening on port ${port}`);
 app.listen(port);
